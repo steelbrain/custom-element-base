98a8a022c8cf5feedf8ba92ac7bc0b215c5561e2
steelbrain <aneesiqbalbhatti@gmail.com>
2015-10-22 21:21:55 -0600

    :new: Introduce new config mechanism

--- base.js
+++ base.js
@@ -1,28 +1,40 @@
 'use strict'
 
-function registerCustomElement({name, created, attached, detached, attributeChanged, config: {}}) {
+// @Compiler-Transpile "true"
+// @Compiler-Compress "true"
+// @Compiler-Output "base.min.js"
+
+function registerCustomElement({name, created, attached, detached, attributeChanged, config = {}}) {
   const element = Object.create(HTMLElement.prototype)
   element.createdCallback = function() {
-    const elementConfig = this.config = {__init: false}
+    const elementConfig = this.__config = {__init: false}
     const element = this
     for (let name in config) {
       if (config.hasOwnProperty(name)) {
         const current = config[name]
-        current.default = current.default || null
-        Object.defineProperty(elementConfig, name, {
-          get: function() {
-            return elementConfig[`_${name}`] || element.getAttribute(name) || current.default
-          },
+        if (typeof current.type === 'undefined') {
+          current.type = String
+        }
+        if (current.default) {
+          elementConfig[name] = registerCustomElement.normalizeType(current.type, current.default)
+        }
+        Object.defineProperty(this, name, {
           set: function(value) {
-            elementConfig[`_${name}`] = value
+            if (current.type !== Object) {
+              element.setAttribute(name, value)
+            }
+            elementConfig[name] = registerCustomElement.normalizeType(current.type, value)
+          },
+          get: function() {
+            return elementConfig[name] || registerCustomElement.normalizeType(current.type, element.getAttribute(name))
           }
         })
       }
     }
   }
   element.attachedCallback = function() {
-    if (!this.config.__init) {
-      this.config.__init = true
+    if (!this.__config.__init) {
+      this.__config.__init = true
       created.call(this)
     }
     if (typeof attached !== 'undefined') {
@@ -34,12 +46,21 @@ function registerCustomElement({name, created, attached, detached, attributeChan
       detached.call(this)
     }
   }
-  element.attributeChangedCallback = function({attrName, oldVal, newVal}) {
-    if (typeof attributeChanged !== 'undefined') {
-      attributeChanged.call(this, {attrName, new: newVal, old: oldVal})
+  element.attributeChangedCallback = function(attrName, oldVal, newVal) {
+    if (attrName in config) {
+      this[attrName] = newVal
+    } else {
+      if (typeof attributeChanged !== 'undefined') {
+        attributeChanged.call(this, {attrName, new: newVal, old: oldVal})
+      }
     }
   }
   return document.registerElement(name, {prototype: element});
+}
+registerCustomElement.normalizeType = function(type, value) {
+  if (value instanceof type) {
+    return value
+  } else return type(value)
 }
 
 if (typeof module !== 'undefined') {
